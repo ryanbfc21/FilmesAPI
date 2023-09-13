656c2dc5ddf4f5ba3c69b904939c2444994699f0
Ryan Batista de Freitas Cordeiro <ryanbfc@live.com>
2023-09-12 21:37:13 -0300

    commit initial

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,49 @@
+﻿using FilmesAPI.Models;
+using FilmesAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            FilmeRepository filmeRepository = new FilmeRepository();
+
+            filme = filmeRepository.GravarFilme(filme);
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new {id = filme.Id}, filme);
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 1000)
+        {
+            FilmeRepository filmeRepository = new FilmeRepository();
+            return filmeRepository.ListFilmes().Skip(skip).Take(take);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+            FilmeRepository filmeRepository = new FilmeRepository();
+            var filme = filmeRepository.FindFilmePorId(id);
+
+            if (filme != null)
+            {
+                return Ok(filme);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+    }
+}
---
+++ Repository/FilmeRepository.cs
@@ -0,0 +1,81 @@
+﻿using FilmesAPI.Models;
+using Newtonsoft.Json;
+
+namespace FilmesAPI.Repository
+{
+    public class FilmeRepository
+    {
+        static readonly string nomeArquivo = "Filmes.json";
+        static readonly string diretorio = @"C:\Users\ryanb\Área de Trabalho";
+        static readonly string caminhoArquivo = Path.Combine(diretorio, nomeArquivo);
+
+        public List<Filme> ListFilmes()
+        {
+            List<Filme> filmes = new List<Filme>();
+
+            if (Directory.Exists(diretorio))
+            {
+                string[] arquivos = Directory.GetFiles(diretorio);
+                string arquivo = arquivos.FirstOrDefault(x => x == caminhoArquivo);
+
+                if (!string.IsNullOrEmpty(arquivo))
+                {
+                    var reader = new StreamReader(arquivo);
+                    string text = reader.ReadToEnd();
+                    reader.Dispose();
+
+                    if (!string.IsNullOrEmpty(text))
+                    {
+                        filmes = JsonConvert.DeserializeObject<List<Filme>>(text);
+                    }
+                }
+            }
+            return filmes;
+        }
+
+        public Filme GravarFilme(Filme filme)
+        {
+            FilmeRepository filmeRepository = new FilmeRepository();
+
+            var filmes = filmeRepository.ListFilmes();
+            filme.Id = filmeRepository.GetLastFilmeId(filmes) + 1;
+
+            filmes.Add(filme);
+
+            using (StreamWriter writer = new StreamWriter(caminhoArquivo))
+            {
+
+                JsonSerializerSettings settings = new JsonSerializerSettings
+                {
+                    Formatting = Formatting.Indented,
+                    StringEscapeHandling = StringEscapeHandling.Default
+                };
+
+                writer.WriteLine(JsonConvert.SerializeObject(filmes, settings));
+            }
+
+            return filme;
+        }
+
+        public int GetLastFilmeId(List<Filme>filmes)
+        {
+            var ultimoFilme = filmes.LastOrDefault();
+            return ultimoFilme != null ? ultimoFilme.Id : 0;
+        }
+
+        public Filme FindFilmePorId(int id)
+        {
+            FilmeRepository filmeRepository = new FilmeRepository();
+
+            var filmes = filmeRepository.ListFilmes();
+
+            if (filmes.Count > 0)
+            {
+                return filmes.FirstOrDefault(x => x.Id == id);
+            }
+
+            return null;
+        }
+
+    }
+}
