58b5b1be906593e7de1054852bec7b8ae6156b9d
Ryan Batista de Freitas Cordeiro <ryanbfc@live.com>
2023-09-18 08:31:41 -0300

    release: Documentando a api com comentários no swagger

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -26,6 +26,14 @@ namespace FilmesAPI.Controllers
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// Adiciona um filme ao banco de dados
+        /// </summary>
+        /// <param name="filmeDto">
+        /// Objeto com os campos necessários para criação de um filme
+        /// </param>
+        /// <returns>IActionResult</returns>
+        /// <response code = "201">Caso inserção seja feita com sucesso</response>
         [HttpPost]
         public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
         {
